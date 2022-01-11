10820c4ca0515bb97a5d917b0e47cdde9e475a5f
Hossein Sokhandan <iamsokhandan@gmail.com>
2022-01-11 09:02:37 +0330

    main.py with startupevent

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
-from routers import member
-
-from src import settings
-from src.routers import document, permission
+from src.settings import settings
+from src.routers import document, permission, member
+from src.db import init_db
 
 
 app = FastAPI()
 app.include_router(member.router, prefix="/member")
 app.include_router(document.router, prefix="/document")
 app.include_router(permission.router, prefix="/permission")
+
+
+@app.on_event("startup")
+async def startup_event():
+    init_db(app)
