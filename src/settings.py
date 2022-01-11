9eb8cd5652d006ebe65f841505400b9a97a5d322
Hossein Sokhandan <iamsokhandan@gmail.com>
2022-01-11 09:00:43 +0330

    settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,32 @@
-import os
+from typing import Any, Dict, Optional
+from pydantic import BaseSettings, PostgresDsn, validator, Field
 
-db_host = os.getenv("DB_HOST", "127.0.0.1")
-db_password = os.getenv("DB_PASSWD", "123456")
-db_user = os.getenv("DB_USER", "auth")
-db_port = os.getenv("DB_PORT", "5432")
+
+class Settings(BaseSettings):
+    DB_HOST: str = Field(default="127.0.0.1")
+    DB_PORT: str = Field(default="5432")
+    DB_NAME: str = Field(default="foo")
+    DB_USER: str = Field(default="auth")
+    DB_PASSWD: str = Field(default="123456")
+
+    DATABASE_URI: Optional[PostgresDsn] = None
+
+    @validator("DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("DB_USER"),
+            password=values.get("DB_PASSWD"),
+            host=values.get("DB_HOST"),
+            port=values.get("DB_PORT"),
+            path=f"/{values.get('DB_NAME')}",
+        )
+
+    class Config:
+        case_sensitive = True
+        env_file = ".env"
+
+
+settings = Settings()
